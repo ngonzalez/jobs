17005515c2116869157c436e63b2127a71c34b28
Nicolas Gonzalez <nicolasgonzalez180@gmail.com>
2022-02-04 02:13:29 +0100

    Fix pg types

--- backend/level1/config/boot.rb
+++ backend/level1/config/boot.rb
@@ -4,3 +4,4 @@ require "bundler/setup" # Set up gems listed in the Gemfile.
 require "bootsnap/setup" # Speed up boot time by caching expensive operations.
 
 require File.expand_path('../config/config.rb', __dir__)
+require File.expand_path('../config/types.rb', __dir__)
